dcb0d688c8462eda0d3b5829eea010da10002b69
DanDon01 <daniel.donnelly@blueyonder.co.uk>
2025-01-21 23:15:11 +0000

    Set invert option to False and enable BGR mode in chilli screensaver

--- examples/chilli_screensaver.py
+++ examples/chilli_screensaver.py
@@ -42,7 +42,8 @@ if __name__ == "__main__":
         backlight=12,
         rotation=270,
         spi_speed_hz=80000000,
-        invert=True
+        invert=False
+        bgr=True
     )
     display.begin()
     draw_chilli_animation(display, icons, stop_event, display_lock)
