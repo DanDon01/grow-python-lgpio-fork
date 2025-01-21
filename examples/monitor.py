898c34bf52bac3385578798ea20f209d67c14fc4
DanDon01 <daniel.donnelly@blueyonder.co.uk>
2025-01-21 22:52:46 +0000

    Update main function parameters in monitor.py to use RGB format instead of BGR

--- examples/monitor.py
+++ examples/monitor.py
@@ -1207,7 +1207,7 @@ def main():
             backlight=12,    # GPIO 12 => Pin 32
             rotation=270,
             spi_speed_hz=10000000,
-            colour_order=ST7735.BGR,
+            rgb=False,
             invert=False
         )
 
